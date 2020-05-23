22ccd6b9cb999283938f722b70146c6d2e5f9ae4
Philip Paeps <philip@trouble.is>
2020-05-23 15:18:19 +0800

    aqm: prototype air quality monitoring page
    Pulls our sensor data from openSenseMap and presents it in a vaguely
    readable format for displaying on the big teevee in the hackerspace.

---
+++ aqm.js
@@ -0,0 +1,39 @@
+function updateValues(json) {
+  $.each(json.sensors, function(i, val) {
+    switch (val.title) {
+    case "PM10":
+      $("#pm10").html(val.lastMeasurement.value + " " + val.unit);
+      break;
+    case "PM2.5":
+      $("#pm25").html(val.lastMeasurement.value + " " + val.unit);
+      break;
+    case "Temperatur":
+      $("#temperature").html(val.lastMeasurement.value + val.unit);
+      break;
+    case "rel. Luftfeuchte":
+      $("#rh").html(val.lastMeasurement.value + val.unit);
+      break;
+    case "Luftdruck":
+      $("#pressure").html(val.lastMeasurement.value + " " + val.unit);
+      break;
+    }
+  });
+}
+
+function getAqmData() {
+  $.getJSON("https://api.opensensemap.org/boxes/5e1afe5fe620bc001a21a9e4")
+  .fail(function(xhr, status, errorThrown) {
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+    $("#timestamp").addClass("danger");
+    $("#timestamp").html("UNKNOWN");
+  })
+  .done(function(json) {
+    $("#timestamp").addClass("okay");
+    $("#timestamp").html(json.updatedAt);
+    updateValues(json);
+  })
+}
+
+getAqmData();
